25f82d3d130d58657a4679f78cf2a5187d1e87a0
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-20 19:26:24 -0400

    remove post middleware from app, add to controller

--- src/app.js
+++ src/app.js
@@ -18,40 +18,6 @@ app.use("/pastes/:pasteId", (req, res, next) => {
 
 app.use("/pastes", pastesRouter); // Note: app.use
 
-// New middleware function to validate the request body
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next();
-  }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
-
-let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
-
-app.post(
-  "/pastes",
-  bodyHasTextProperty, // Add validation middleware function
-  (req, res) => {
-    // Route handler no longer has validation code.
-    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-    const newPaste = {
-      id: ++lastPasteId, // Increment last id then assign as the current ID
-      name,
-      syntax,
-      exposure,
-      expiration,
-      text,
-      user_id,
-    };
-    pastes.push(newPaste);
-    res.status(201).json({ data: newPaste });
-  }
-);
-
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
