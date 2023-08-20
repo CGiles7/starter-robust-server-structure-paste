31c78a6a08b966c042004aa5cc7ed67a108a7470
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-20 19:30:05 -0400

    add new functions to controller, export

--- src/pastes/pastes.controller.js
+++ src/pastes/pastes.controller.js
@@ -6,16 +6,50 @@ function list(req, res) {
 
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0)
 
-function bodyHasTextProperty(req, res, next) {
-  const { data: { text } = {} } = req.body;
-  if (text) {
-    return next();
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+      const { data = {} } = req.body;
+      if (data[propertyName]) {
+        return next();
+      }
+      next({ status: 400, message: `Must include a ${propertyName}` });
+    };
   }
-  next({
-    status: 400,
-    message: "A 'text' property is required.",
-  });
-}
+
+  function exposurePropertyIsValid(req, res, next) {
+    const { data: { exposure } = {} } = req.body;
+    const validExposure = ["private", "public"];
+    if (validExposure.includes(exposure)) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Value of the 'exposure' property must be one of ${validExposure}. Received: ${exposure}`,
+    });
+  }
+  
+  function syntaxPropertyIsValid(req, res, next) {
+    const { data: { syntax } = {} } = req.body;
+    const validSyntax = ["None", "Javascript", "Python", "Ruby", "Perl", "C", "Scheme"];
+    if (validSyntax.includes(syntax)) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Value of the 'syntax' property must be one of ${validSyntax}. Received: ${syntax}`,
+    });
+  }
+  
+  function expirationIsValidNumber(req, res, next){
+    const { data: { expiration }  = {} } = req.body;
+    if (expiration <= 0 || !Number.isInteger(expiration)){
+        return next({
+            status: 400,
+            message: `Expiration requires a valid number`
+        });
+    }
+    next();
+  }
 
 function create(req, res) {
   const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
@@ -33,10 +67,17 @@ function create(req, res) {
 }
 
 module.exports = {
-  create: [bodyHasTextProperty, create],
-  list,
-};
-
-module.exports = {
-  list,
-};
+    create: [
+        bodyDataHas("name"),
+        bodyDataHas("syntax"),
+        bodyDataHas("exposure"),
+        bodyDataHas("expiration"),
+        bodyDataHas("text"),
+        bodyDataHas("user_id"),
+        exposurePropertyIsValid,
+        syntaxPropertyIsValid,
+        expirationIsValidNumber,
+        create
+    ],
+    list,
+  };
